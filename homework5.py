e2de31c5ec4908e03aa507bc9e6d5c5e47beffac
Kurban2132 <k.kokanov@yandex.ru>
2024-07-23 18:52:57 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 4, 5, 6, True, "Hello World"
+print(immutable_var)
+print(immutable_var[0])
+mutable_list = [1, 2, 3, 4], 5, 6, "a", "b", "Modified"
+print(mutable_list)
+mutable_list[0][3] = 3
+print(mutable_list)
